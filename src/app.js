ac459c96432ce4bad62f3069f65f0a349ae4f0f7
Sahinul <Sahinul.sot010114@pwioi.com>
2025-07-15 10:18:13 +0530

    play with routes

--- src/app.js
+++ src/app.js
@@ -3,19 +3,27 @@ const app = express();
 
 
 
-app.use("/",(req,res)=>{
-    res.send("hello world from the daashboard");
-});
 app.use("/hello",(req,res)=>{
     res.send("hello world from the hello");
 });
-app.use("/test",(req,res)=>{
-    res.send("hello world from the test");
-});
-app.use("/data",(req,res)=>{
-    res.send("hello world from the data");
+
+
+app.get('/user',(req,res)=>{
+    res.send("got the data from db");
+
 });
 
+app.post("/user",(req,res)=>{
+    res.send("data saved to the db");
+});
+app.delete("/user",(req,res)=>{
+    res.send("deleted the user data ");
+});
+
+
+app.use("/",(req,res)=>{
+    res.send("hello world from the daashboard");
+});
 
 app.listen(5000,()=>{
     console.log("server is listening on port 5000");
